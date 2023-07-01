5546fee2cc17615d6ee7df778eced8f251083ada
Jayden Grubb <jaydengrubb1@gmail.com>
2023-07-01 22:23:08 +1000

    feat(kprintf): Implemented several formatters
    Implemented several of the arguement formatters used in kprintf. These
    include char (c), string (s), signed int (i, d), octal (o), unsigned int
    (u), hexadecimal (x, X), pointer (p) and bytes written (n).

--- kernel/kprintf.cpp
+++ kernel/kprintf.cpp
@@ -13,18 +13,102 @@
 
 #include <kernel/kprintf.hpp>
 #include <kernel/uart.hpp>
+#include <lib/stdlib.h>
+#include <lib/string.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
 int kprintf(const char *__restrict__ format, ...) {
 	UART comm(UART::COM1);
-	// TODO Implement format arguments
-
 	int count = 0;
-	char *str = (char *)format;
-	while (*str != '\0') {
-		comm.write(*str);
-		count++;
-		str++;
+	bool upper = false;
+	char buffer[32];
+
+	va_list args;
+	va_start(args, format);
+
+	for (size_t i = 0; format[i] != '\0'; i++) {
+		if (format[i] == '%') {
+			switch (format[++i]) {
+				case 'c': {
+					char c = (char)va_arg(args, int);
+					comm.write(c);
+					count++;
+					break;
+				}
+				case 's': {
+					char *s = va_arg(args, char *);
+					count += kprintf(s);
+					break;
+				}
+				case 'i':
+				case 'd': {
+					int32_t d = va_arg(args, int32_t);
+					itoa(d, buffer, 10);
+					count += kprintf(buffer);
+					break;
+				}
+				case 'o': {
+					int32_t o = va_arg(args, int32_t);
+					uitoa(o, buffer, 8);
+					count += kprintf(buffer);
+					break;
+				}
+				case 'u': {
+					uint32_t u = va_arg(args, uint32_t);
+					uitoa(u, buffer, 10);
+					count += kprintf(buffer);
+					break;
+				}
+				case 'X':
+					upper = true;
+				case 'x': {
+					int32_t x = va_arg(args, int32_t);
+					uitoa(x, buffer, 16);
+					if (upper) {
+						for (size_t i = 0; buffer[i] != '\0'; i++) {
+							if (buffer[i] >= 'a' && buffer[i] <= 'z') {
+								buffer[i] -= 32;
+							}
+						}
+					}
+					count += kprintf(buffer);
+					break;
+				}
+				case 'p': {
+					void *p = va_arg(args, void *);
+					ultoa((uint64_t)p, buffer, 16);
+					int len = strlen(buffer);
+					int pad = 16 - len - 1;
+					memmove(buffer + pad, buffer, len);
+					memset(buffer, '0', pad);
+					count += kprintf("0x");
+					count += kprintf(buffer);
+					break;
+				}
+				case 'n': {
+					int *n = va_arg(args, int *);
+					*n = count;
+					break;
+				}
+				case '%': {
+					comm.write('%');
+					count++;
+					break;
+				}
+				default: {
+					comm.write(format[i]);
+					count++;
+					break;
+				}
+			}
+		} else {
+			comm.write(format[i]);
+			count++;
+		}
 	}
+
+	va_end(args);
 
 	return count;
 }
