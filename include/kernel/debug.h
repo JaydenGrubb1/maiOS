babcf0a3a20d9360f908586562ecdc77710853e3
Jayden Grubb <jaydengrubb1@gmail.com>
2023-07-11 20:38:40 +1000

    feat(debug): Merged logger and trace into debug
    Merged the logger and trace header/functions into a single debug
    namespace. Also made LOG_*** into actual functions Debug::log_***
    instead of header macros.

---
+++ include/kernel/debug.h
@@ -0,0 +1,68 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2023-07-11
+ * @brief // DOC
+ *
+ * Copyright (c) 2023, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+namespace Debug {
+	/**
+	 * @brief Log a message to output
+	 *
+	 * @param format The format string
+	 * @param ... The format arguments
+	 */
+	void log(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+	/**
+	 * @brief Log a failure message to output
+	 *
+	 * @param format The format string
+	 * @param ... The format arguments
+	 */
+	void log_failure(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+	/**
+	 * @brief Log an info message to output
+	 *
+	 * @param format The format string
+	 * @param ... The format arguments
+	 */
+	void log_info(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+	/**
+	 * @brief Log an ok message to output
+	 *
+	 * @param format The format string
+	 * @param ... The format arguments
+	 */
+	void log_ok(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+	/**
+	 * @brief Log a warning message to output
+	 *
+	 * @param format The format string
+	 * @param ... The format arguments
+	 */
+	void log_warning(const char *format, ...) __attribute__((format(printf, 1, 2)));
+
+	/**
+	 * @brief Print a stack trace to output
+	 *
+	 */
+	void trace(void);
+
+	/**
+	 * @brief Print a stack trace to output
+	 *
+	 * @param max_frames The maximum number of frames to print
+	 */
+	void trace(unsigned int max_frames);
+}
---
+++ src/kernel/debug.cpp
@@ -0,0 +1,80 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2023-07-11
+ * @brief // DOC
+ *
+ * Copyright (c) 2023, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <kernel/debug.h>
+#include <kernel/kprintf.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+void Debug::log(const char *__restrict__ format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	kputs("         ");
+	kvprintf(format, ap);
+	kputchar('\n');
+	va_end(ap);
+}
+
+void Debug::log_failure(const char *__restrict__ format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	kputs("[\u001b[31m FAIL \u001b[0m] ");
+	kvprintf(format, ap);
+	kputchar('\n');
+	va_end(ap);
+}
+
+void Debug::log_info(const char *__restrict__ format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	kputs("[\u001b[36m INFO \u001b[0m] ");
+	kvprintf(format, ap);
+	kputchar('\n');
+	va_end(ap);
+}
+
+void Debug::log_ok(const char *__restrict__ format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	kputs("[\u001b[32m  OK  \u001b[0m] ");
+	kvprintf(format, ap);
+	kputchar('\n');
+	va_end(ap);
+}
+
+void Debug::log_warning(const char *__restrict__ format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	kputs("[\u001b[33m WARN \u001b[0m] ");
+	kvprintf(format, ap);
+	kputchar('\n');
+	va_end(ap);
+}
+
+void Debug::trace(void) {
+	Debug::trace(-1);
+}
+
+void Debug::trace(unsigned int max_frames) {
+	uint64_t *base_ptr = (uint64_t *)__builtin_frame_address(0);
+
+	kprintf("Stack trace:\n");
+	unsigned int count = 0;
+	while (base_ptr && count < max_frames) {
+		uint64_t return_address = *(base_ptr + 1);
+		kprintf("frame #%d: %#.16lx => '%s' (%#.16lx) + %d\n", count++, return_address, "unknown", 0, 0);
+		base_ptr = (uint64_t *)*base_ptr;
+	}
+
+	// TODO Find kernel symbols and print them, e.g.
+	// frame #0: 0x000000000010229c => 'invalid_opcode' (0x000000000010226a) + 32
+}
