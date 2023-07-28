fcbc5809b5b3eac3a49bfc5cd4b3365a30d59b8e
Jayden Grubb <jaydengrubb1@gmail.com>
2023-07-28 22:22:50 +1000

    fix: Cache toolchain compiler path
    Updated the toolchain file to cache the compiler paths.

--- tools/toolchain.cmake
+++ tools/toolchain.cmake
@@ -1,12 +1,12 @@
 # set(CMAKE_SYSTEM_NAME Mai-OS)
 # set(CMAKE_SYSTEM_VERSION 0.0.1)
 # set(CMAKE_SYSTEM_PROCESSOR x86_64)
-# FIXME CMAKE_SYSTEM_NAME is not set to Mai-OS
+# TODO Change this to Mai-OS
 
-set(CMAKE_C_COMPILER "/home/jayden/mai-os/tools/compiler/bin/x86_64-elf-gcc")
-set(CMAKE_CXX_COMPILER "/home/jayden/mai-os/tools/compiler/bin/x86_64-elf-gcc")
-set(CMAKE_ASM_COMPILER "/usr/bin/nasm")
-set(CMAKE_LINKER "/home/jayden/mai-os/tools/compiler/bin/x86_64-elf-gcc")
+set(CMAKE_C_COMPILER ${CMAKE_CURRENT_LIST_DIR}/compiler/bin/x86_64-elf-gcc CACHE PATH "" FORCE)
+set(CMAKE_CXX_COMPILER ${CMAKE_CURRENT_LIST_DIR}/compiler/bin/x86_64-elf-gcc CACHE PATH "" FORCE)
+set(CMAKE_ASM_COMPILER /usr/bin/nasm)
+set(CMAKE_LINKER ${CMAKE_CURRENT_LIST_DIR}/compiler/bin/x86_64-elf-gcc CACHE PATH "" FORCE)
 
 set(CMAKE_C_COMPILER_WORKS true)
 set(CMAKE_CXX_COMPILER_WORKS true)
