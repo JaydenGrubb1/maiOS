c18162b9054e4e89b836b6d1e27234a486c83dcb
Jayden Grubb <jaydengrubb1@gmail.com>
2023-08-11 04:18:25 +1000

    feat(libc++): Implemented most <pair> constructors
    Implemented most of the constructors for the kstd::pair class. At this
    point the class is already usable, the rest of the functions are mostly
    just for convenience.

--- include/lib/libc++/pair.h
+++ include/lib/libc++/pair.h
@@ -11,36 +11,69 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <type_traits>
+#include <utility> // VERIFY Use this or custom <utility>?
+// #include <lib/libc++/utility.h>
+
 namespace kstd {
 	/**
 	 * @brief Class template for storing a pair of heterogeneous values
 	 *
 	 * @tparam T1 The type of the first value
 	 * @tparam T2 The type of the second value
-	 * 
+	 *
 	 * @link https://en.cppreference.com/w/cpp/utility/pair @endlink
 	 */
 	template <typename T1, typename T2>
 	class pair {
 	  public:
-		constexpr explicit pair();
+		T1 first;
+		T2 second;
 
-		constexpr explicit pair(const T1 &x, const T2 &y);
+#pragma region Constructors
+		constexpr explicit pair()
+			requires(std::is_default_constructible_v<T1> && std::is_default_constructible_v<T2>)
+			: first(), second() {}
+		// TODO check implicit default constructor
+
+		constexpr explicit(!std::is_convertible_v<const T1 &, T1> || !std::is_convertible_v<const T2 &, T2>)
+			pair(const T1 &x, const T2 &y)
+			requires(std::is_copy_constructible_v<T1> && std::is_copy_constructible_v<T2>)
+			: first(x), second(y) {}
 
 		template <typename U1 = T1, typename U2 = T2>
-		constexpr explicit pair(U1 &&x, U2 &&y);
+		constexpr explicit(!std::is_convertible_v<U1, T1> || !std::is_convertible_v<U2, T2>)
+			pair(U1 &&x, U2 &&y)
+			requires(std::is_constructible_v<T1, U1> && std::is_constructible_v<T2, U2>)
+			: first(std::forward<U1>(x)), second(std::forward<U2>(y)) {}
 
 		template <typename U1, typename U2>
-		constexpr explicit pair(pair<U1, U2> &p);
+		constexpr explicit(!std::is_convertible_v<const U1 &, T1> || !std::is_convertible_v<const U2 &, T2>)
+			pair(pair<U1, U2> &p)
+			requires(std::is_constructible_v<T1, const U1 &> && std::is_constructible_v<T2, const U2 &>)
+			: first(p.first), second(p.second) {}
+		// TODO delete if initialization would bind to a reference to a temporary
 
 		template <typename U1, typename U2>
-		constexpr explicit pair(const pair<U1, U2> &p);
+		constexpr explicit(!std::is_convertible_v<U1 &, T1> || !std::is_convertible_v<U2 &, T2>)
+			pair(const pair<U1, U2> &p)
+			requires(std::is_constructible_v<T1, U1 &> && std::is_constructible_v<T2, U2 &>)
+			: first(p.first), second(p.second) {}
+		// TODO delete if initialization would bind to a reference to a temporary
 
 		template <typename U1, typename U2>
-		constexpr explicit pair(pair<U1, U2> &&p);
+		constexpr explicit(!std::is_convertible_v<U1 &&, T1> || !std::is_convertible_v<U2 &&, T2>)
+			pair(pair<U1, U2> &&p)
+			requires(std::is_constructible_v<T1, U1 &&> && std::is_constructible_v<T2, U2 &&>)
+			: first(std::forward<U1>(p.first)), second(std::forward<U2>(p.second)) {}
+		// TODO delete if initialization would bind to a reference to a temporary
 
 		template <typename U1, typename U2>
-		constexpr explicit pair(const pair<U1, U2> &&p);
+		constexpr explicit(!std::is_convertible_v<const U1 &&, T1> || !std::is_convertible_v<const U2 &&, T2>)
+			pair(const pair<U1, U2> &&p)
+			requires(std::is_constructible_v<T1, const U1 &&> && std::is_constructible_v<T2, const U2 &&>)
+			: first(std::forward<U1>(p.first)), second(std::forward<U2>(p.second)) {}
+		// TODO delete if initialization would bind to a reference to a temporary
 
 		// template <pair-like P>
 		// constexpr explicit pair(P &&u);
@@ -50,8 +83,9 @@ namespace kstd {
 		// constexpr pair(std::piecewise_construct_t, tuple<Args1...> arg1, tuple<Args2...> arg2);
 		// // TODO what is piecewise_construct_t?
 
-		pair(const pair &p) = default;
+		constexpr pair(const pair &p) = default;
 
-		pair(pair &&p) = default;
+		constexpr pair(pair &&p) = default;
+#pragma endregion
 	};
 }
