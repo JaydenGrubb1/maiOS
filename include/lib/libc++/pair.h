db6eb278a938618a5cdd3dbe58c8ce61f79c64df
Jayden Grubb <jaydengrubb1@gmail.com>
2023-08-10 23:25:15 +1000

    feat(libc++): Added basic pair and tuple headers
    Added some basic pair and tuple headers. Neither are implemented and
    only contain the constructors. They also both depend on each other, so
    some re-shuffling of declarations will likely be needed. Additionally,
    paid should be defined in utility, but it might make sense to define it
    as a standalone header as well.

---
+++ include/lib/libc++/pair.h
@@ -0,0 +1,55 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2023-08-10
+ * @brief Class template for storing a pair of heterogeneous values
+ * @note https://en.cppreference.com/w/cpp/utility/pair @endlink
+ *
+ * Copyright (c) 2023, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace kstd {
+	/**
+	 * @brief Class template for storing a pair of heterogeneous values
+	 *
+	 * @tparam T1 The type of the first value
+	 * @tparam T2 The type of the second value
+	 */
+	template <typename T1, typename T2>
+	class paid {
+	  public:
+		constexpr explicit pair();
+
+		constexpr explicit pair(const T1 &x, const T2 &y);
+
+		template <typename U1 = T1, typename U2 = T2>
+		constexpr explicit pair(U1 &&x, U2 &&y);
+
+		template <typename U1, typename U2>
+		constexpr explicit pair(pair<U1, U2> &p);
+
+		template <typename U1, typename U2>
+		constexpr explicit pair(const pair<U1, U2> &p);
+
+		template <typename U1, typename U2>
+		constexpr explicit pair(pair<U1, U2> &&p);
+
+		template <typename U1, typename U2>
+		constexpr explicit pair(const pair<U1, U2> &&p);
+
+		// template <pair-like P>
+		// constexpr explicit pair(P &&u);
+		// // TODO what is pair-like?
+
+		// template <typename... Args1, typename... Args2>
+		// constexpr pair(std::piecewise_construct_t, tuple<Args1...> arg1, tuple<Args2...> arg2);
+		// // TODO what is piecewise_construct_t?
+
+		pair(const pair &p) = default;
+
+		pair(pair &&p) = default;
+	};
+}
---
+++ include/lib/libc++/tuple.h
@@ -0,0 +1,64 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2023-08-10
+ * @brief Class template for a fixed-size collection of heterogeneous values
+ * @link https://en.cppreference.com/w/cpp/utility/tuple @endlink
+ *
+ * Copyright (c) 2023, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+namespace kstd {
+	/**
+	 * @brief Class template for a fixed-size collection of heterogeneous values
+	 *
+	 * @tparam Types The types of the values to be stored
+	 */
+	template <typename... Types>
+	class tuple {
+	  public:
+		constexpr explicit tuple();
+
+		constexpr explicit tuple(const Types &...);
+
+		template <typename... UTypes>
+		constexpr explicit tuple(UTypes &&...args);
+
+		template <typename... UTypes>
+		constexpr explicit tuple(tuple<UTypes...> &other);
+
+		template <typename... UTypes>
+		constexpr explicit tuple(const tuple<UTypes...> &other);
+
+		template <typename... UTypes>
+		constexpr explicit tuple(tuple<UTypes...> &&other);
+
+		template <typename... UTypes>
+		constexpr explicit tuple(const tuple<UTypes...> &&other);
+
+		template <typename U1, typename U2>
+		constexpr explicit tuple(pair<U1, U2> &p);
+
+		template <typename U1, typename U2>
+		constexpr explicit tuple(const pair<U1, U2> &p);
+
+		template <typename U1, typename U2>
+		constexpr explicit tuple(pair<U1, U2> &&p);
+
+		template <typename U1, typename U2>
+		constexpr explicit tuple(const std::pair<U1, U2> &&p);
+
+		// template <tuple-like UTuple>
+		// constexpr explicit tuple(UTuple &&u);
+		// // TODO what is tuple-like?
+
+		tuple(const tuple &other) = default;
+
+		tuple(tuple &&other) = default;
+
+		// TODO allocator extended constructors ?
+	};
+}
