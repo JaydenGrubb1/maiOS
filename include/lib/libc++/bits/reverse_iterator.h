991bde361645aa298ca9eac1fdc7bb948afb98fe
Jayden Grubb <jaydengrubb1@gmail.com>
2023-08-10 13:09:25 +1000

    feat(libc++): Added operator+/- and make_ builder
    Added the +/- operators to the reverse_iterator header allowing the user
    to advance an iterator or find the difference between iterators.
    Additionally added make_reverse_iterator for constructing
    reverse_iterators.

--- include/lib/libc++/bits/reverse_iterator.h
+++ include/lib/libc++/bits/reverse_iterator.h
@@ -174,47 +174,97 @@ namespace kstd {
 			return *this;
 		}
 #pragma endregion
+
+		// TODO iter_move
+		// https://en.cppreference.com/w/cpp/iterator/reverse_iterator/iter_move
+		// friend constexpr std::iter_rvalue_reference_t<T> iter_move(const reverse_iterator &i) {
+		// }
+
+		// TODO iter_swap
+		// https://en.cppreference.com/w/cpp/iterator/reverse_iterator/iter_swap
+		// template <std::indirectly_swappable<T> T2>
+		// friend constexpr void iter_swap(const reverse_iterator &x, const reverse_iterator<T2> &y) {
+		// }
 	};
 
 // Comparison Operators
 // https://en.cppreference.com/w/cpp/iterator/reverse_iterator
 #pragma region Comparison Operators
 	template <class T, class U>
-	constexpr bool operator==(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator==(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() == rhs.base();
 	}
 
 	template <class T, class U>
-	constexpr bool operator!=(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator!=(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() != rhs.base();
 	}
 
 	template <class T, class U>
-	constexpr bool operator<(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator<(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() > rhs.base();
 	}
 
 	template <class T, class U>
-	constexpr bool operator<=(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator<=(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() >= rhs.base();
 	}
 
 	template <class T, class U>
-	constexpr bool operator>(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator>(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() < rhs.base();
 	}
 
 	template <class T, class U>
-	constexpr bool operator>=(const kstd::reverse_iterator<T> &lhs, const kstd::reverse_iterator<U> &rhs) {
+	constexpr bool operator>=(const reverse_iterator<T> &lhs, const reverse_iterator<U> &rhs) {
 		return lhs.base() <= rhs.base();
 	}
 
 	// TODO 3-way comparison
 #pragma endregion
 
-	// TODO operator+
-	// TODO operator-
-	// TODO iter_move
-	// TODO iter_swap
-	// TODO make_reverse_iterator
+	/**
+	 * @brief Advances the given reverse_iterator by the given amount
+	 *
+	 * @tparam T The type of the reverse_iterator
+	 * @param n The amount to advance the reverse_iterator by
+	 * @param iter The reverse_iterator to advance
+	 * @return The advanced reverse_iterator
+	 *
+	 * @link https://en.cppreference.com/w/cpp/iterator/reverse_iterator/operator%2B @endlink
+	 */
+	template <class T>
+	constexpr reverse_iterator<T> operator+(std::ptrdiff_t n, const reverse_iterator<T> &iter) {
+		return reverse_iterator<T>(iter.base() - n);
+	}
+
+	/**
+	 * @brief Calculates the distance between two reverse_iterators
+	 *
+	 * @tparam I1 The type of the first reverse_iterator
+	 * @tparam I2 The type of the second reverse_iterator
+	 * @param lhs The first reverse_iterator
+	 * @param rhs The second reverse_iterator
+	 * @return The distance between the two reverse_iterators
+	 *
+	 * @link https://en.cppreference.com/w/cpp/iterator/reverse_iterator/operator- @endlink
+	 */
+	template <class T1, class T2>
+	constexpr ptrdiff_t operator-(const reverse_iterator<T1> &lhs, const reverse_iterator<T2> &rhs) {
+		return rhs.base() - lhs.base();
+	}
+
+	/**
+	 * @brief Creates a reverse_iterator from the given iterator
+	 *
+	 * @tparam T The type of the iterator to be reversed
+	 * @param i The iterator to be reversed
+	 * @return A reverse_iterator
+	 *
+	 * @link https://en.cppreference.com/w/cpp/iterator/make_reverse_iterator @endlink
+	 */
+	template <class T>
+	constexpr reverse_iterator<T> make_reverse_iterator(T i) {
+		return reverse_iterator<T>(i);
+	}
 }
