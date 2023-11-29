2f90ba2afac1d37beaea0b72d268fede632cd5ab
Jayden Grubb <jaydengrubb1@gmail.com>
2023-11-29 21:06:34 +1000

    feat(libc++): Implement vector constructors
    Implemented most of the vector class constructors, as well as the
    deconstructor. None of these have been tested yet as their is currently
    no suitable allocator (next step).

--- include/lib/libc++/vector.h
+++ include/lib/libc++/vector.h
@@ -14,8 +14,11 @@
 #pragma once
 
 #include <initializer_list>
+#include <memory> // TODO replace with <lib/libc++/memory.h> ???
 
 #include <lib/libc++/bits/allocator.h>
+#include <lib/libc++/utility.h>
+#include <lib/libc/assert.h>
 
 namespace kstd {
 	/**
@@ -30,32 +33,95 @@ namespace kstd {
 	class vector {
 	  public:
 #pragma region Constructors
-		constexpr vector(void) : _alloc() {}
+		constexpr vector(void) = default;
 
 		constexpr explicit vector(const Alloc &alloc) : _alloc(alloc) {}
 
-		constexpr vector(size_t count, const T &value, const Alloc &alloc = Alloc()) : _alloc(alloc) {}
+		constexpr vector(size_t count, const T &value, const Alloc &alloc = Alloc())
+			: _capacity(count), _size(count), _alloc(alloc) {
+			_data = _alloc.allocate(count);
+			assert(_data != nullptr);
 
-		constexpr explicit vector(size_t count, const Alloc &alloc = Alloc()) : _alloc(alloc) {}
+			for (size_t i = 0; i < count; i++) {
+				_data[i] = value;
+			}
+		}
 
-		template <typename Iter>
-		constexpr vector(Iter first, Iter last, const Alloc &alloc = Alloc()) : _alloc(alloc) {}
+		constexpr explicit vector(size_t count, const Alloc &alloc = Alloc())
+			: _capacity(count), _size(count), _alloc(alloc) {
+			_data = _alloc.allocate(count);
+		}
 
-		constexpr vector(const vector &other) : _alloc(other._alloc) {}
+		// template <typename Iter>
+		// constexpr vector(Iter first, Iter last, const Alloc &alloc = Alloc()) : _alloc(alloc) {}
+		// TODO Add requirements to avoid ambiguity with the above constructor
 
-		constexpr vector(const vector &other, const Alloc &alloc) : _alloc(alloc) {}
+		constexpr vector(const vector &other)
+			: _capacity(other._capacity), _size(other._size), _alloc(other._alloc) {
+			_data = _alloc.allocate(other._capacity);
+			assert(_data != nullptr);
 
-		constexpr vector(vector &&other) noexcept : _alloc(other._alloc) {}
+			for (size_t i = 0; i < other._size; i++) {
+				_data[i] = other._data[i];
+			}
+		}
 
-		constexpr vector(vector &&other, const Alloc &alloc) : _alloc(alloc) {}
+		constexpr vector(const vector &other, const Alloc &alloc)
+			: _capacity(other._capacity), _size(other._size), _alloc(alloc) {
+			_data = _alloc.allocate(other._capacity);
+			assert(_data != nullptr);
 
-		constexpr vector(std::initializer_list<T> init, const Alloc &alloc = Alloc()) : _alloc(alloc) {}
+			for (size_t i = 0; i < other._size; i++) {
+				_data[i] = other._data[i];
+			}
+		}
+
+		constexpr vector(vector &&other) noexcept
+			: _capacity(other._capacity), _size(other._size), _alloc(other._alloc) {
+			_data = other._data;
+			other._data = nullptr;
+		}
+
+		constexpr vector(vector &&other, const Alloc &alloc)
+			: _capacity(other._capacity), _size(other._size), _alloc(alloc) {
+			if (alloc == other._alloc) {
+				_data = other._data;
+				other._data = nullptr;
+			} else {
+				_data = _alloc.allocate(other._capacity);
+				assert(_data != nullptr);
+
+				for (size_t i = 0; i < other._size; i++) {
+					_data[i] = kstd::move(other._data[i]);
+				}
+			}
+		}
+
+		constexpr vector(std::initializer_list<T> list, const Alloc &alloc = Alloc())
+			: _capacity(list.size()), _size(list.size()), _alloc(alloc) {
+			_data = _alloc.allocate(list.size());
+			assert(_data != nullptr);
+
+			for (size_t i = 0; i < list.size(); i++) {
+				_data[i] = list[i];
+			}
+		}
 
 		// TODO Range constructor
 #pragma endregion
 
+		constexpr ~vector(void) {
+			for (size_t i = 0; i < _size; i++) {
+				std::destroy_at(&_data[i]);
+			}
+
+			_alloc.deallocate(_data, _capacity);
+		}
+
 	  private:
-		Alloc _alloc;
-		T *_data;
+		size_t _capacity = 0;
+		size_t _size = 0;
+		Alloc _alloc = {};
+		T *_data = nullptr;
 	};
 }
