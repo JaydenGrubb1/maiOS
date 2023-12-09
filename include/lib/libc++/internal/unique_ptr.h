1afafb31e1228f97e37b16c8707c343358b72e5b
Jayden Grubb <jaydengrubb1@gmail.com>
2023-12-10 00:04:13 +1000

    refactor: Remove deleter from unique_ptr
    Removed the deleter from unique_ptr in order to simplify the
    implementation as deleter will probably never be needed.

--- include/lib/libc++/internal/unique_ptr.h
+++ include/lib/libc++/internal/unique_ptr.h
@@ -17,144 +17,29 @@
 #include <lib/libc++/utility.h>
 
 namespace kstd {
-	/**
-	 * @brief Specifies the deleter type for unique_ptr
-	 *
-	 * @tparam T The type of the pointer to be deleted
-	 */
 	template <typename T>
-	class default_delete {
-	  public:
-		/**
-		 * @brief Construct a new default delete object
-		 *
-		 */
-		constexpr default_delete(void) = default;
-
-		/**
-		 * @brief Construct a new default delete object
-		 *
-		 * @tparam U The underlying type of the default_delete to copy
-		 * @param other The default_delete to copy
-		 */
-		template <typename U>
-		constexpr default_delete(const default_delete<U> &)
-			requires(std::is_convertible_v<U *, T *>)
-		{}
-
-		/**
-		 * @brief Deletes the pointer
-		 *
-		 * @param ptr The pointer to delete
-		 */
-		constexpr void operator()(T *ptr) const {
-			static_assert(!std::is_void_v<T>, "can't delete pointer to incomplete type");
-			static_assert(sizeof(T) > 0, "can't delete pointer to incomplete type");
-			delete ptr;
-		}
-	};
-
-	/**
-	 * @brief Specifies the deleter type for unique_ptr
-	 *
-	 * @tparam T The type of the pointer to be deleted
-	 */
-	template <typename T>
-	class default_delete<T[]> {
-	  public:
-		/**
-		 * @brief Construct a new default delete object
-		 *
-		 */
-		constexpr default_delete(void) = default;
-
-		/**
-		 * @brief Construct a new default delete object
-		 *
-		 * @tparam U The underlying type of the default_delete to copy
-		 * @param other The default_delete to copy
-		 */
-		template <typename U>
-		constexpr default_delete(const default_delete<U[]> &)
-			requires(std::is_convertible_v<U (*)[], T (*)[]>)
-		{}
-
-		/**
-		 * @brief Deletes the pointer
-		 *
-		 * @param ptr The pointer to delete
-		 */
-		constexpr void operator()(T *ptr) const {
-			static_assert(sizeof(T) > 0, "can't delete pointer to incomplete type");
-			delete[] ptr;
-		}
-	};
-
-	template <typename T, typename D = default_delete<T>>
 	class unique_ptr {
 	  private:
 		T *_ptr;
-		[[no_unique_address]] D _deleter;
 
 	  public:
 #pragma region Constructors
-		constexpr unique_ptr(void)
-			requires(std::is_default_constructible_v<D> && !std::is_pointer_v<D>)
-		= default;
+		constexpr unique_ptr(void) = default;
 
-		constexpr unique_ptr(std::nullptr_t)
-			requires(std::is_default_constructible_v<D> && !std::is_pointer_v<D>)
-			: _ptr(nullptr) {}
+		constexpr unique_ptr(std::nullptr_t) : _ptr(nullptr) {}
 
-		constexpr explicit unique_ptr(T *ptr)
-			requires(std::is_default_constructible_v<D> && !std::is_pointer_v<D>)
-			: _ptr(ptr) {}
-
-		constexpr unique_ptr(T *ptr, const D &deleter)
-			requires(!std::is_reference_v<D> && std::is_copy_constructible_v<D>)
-			: _ptr(ptr), _deleter(deleter) {}
-
-		constexpr unique_ptr(T *ptr, D &&deleter)
-			requires(!std::is_reference_v<D> && std::is_move_constructible_v<D>)
-			: _ptr(ptr), _deleter(kstd::move(deleter)) {}
-
-		constexpr unique_ptr(T *ptr, D &deleter)
-			requires(std::is_lvalue_reference_v<D &>)
-			: _ptr(ptr), _deleter(deleter) {}
-
-		constexpr unique_ptr(T *ptr, D &&deleter)
-			requires(std::is_lvalue_reference_v<D &>)
-		= delete;
-
-		constexpr unique_ptr(T *ptr, const D &deleter)
-			requires(std::is_lvalue_reference_v<const D &>)
-			: _ptr(ptr), _deleter(deleter) {}
-
-		constexpr unique_ptr(T *ptr, D &&deleter)
-			requires(std::is_lvalue_reference_v<const D &>)
-		= delete;
+		constexpr explicit unique_ptr(T *ptr) : _ptr(ptr) {}
 
 		constexpr unique_ptr(const unique_ptr &) = delete;
 
-		constexpr unique_ptr(unique_ptr &&other)
-			requires(std::is_move_constructible_v<D>)
-			: _ptr(other._ptr), _deleter(kstd::move(other._deleter)) {
+		constexpr unique_ptr(unique_ptr &&other) : _ptr(other._ptr) {
 			other._ptr = nullptr;
 		}
 
-		template <typename U, typename E>
-		constexpr unique_ptr(unique_ptr<U, E> &&other)
-			requires(std::is_convertible_v<U *, T *> && !std::is_array_v<U> &&
-					 std::is_reference_v<E> && std::is_same_v<E, D>)
-			: _ptr(other._ptr), _deleter(other._deleter) {
-			other._ptr = nullptr;
-		}
-
-		template <typename U, typename E>
-		constexpr unique_ptr(unique_ptr<U, E> &&other)
-			requires(std::is_convertible_v<U *, T *> && !std::is_array_v<U> &&
-					 !std::is_reference_v<E> && std::is_convertible_v<E, D>)
-			: _ptr(other._ptr), _deleter(kstd::forward<E>(other._deleter)) {
+		template <typename U>
+		constexpr unique_ptr(unique_ptr<U> &&other)
+			requires(std::is_convertible_v<U *, T *> && !std::is_array_v<U>)
+			: _ptr(other._ptr) {
 			other._ptr = nullptr;
 		}
 #pragma endregion
@@ -167,41 +52,28 @@ namespace kstd {
 
 		constexpr unique_ptr &operator=(const unique_ptr &) = delete;
 
-		constexpr unique_ptr &operator=(unique_ptr &&other)
-			requires(std::is_move_assignable_v<D>)
-		{
+		constexpr unique_ptr &operator=(unique_ptr &&other) {
 			reset(other.release());
-			_deleter = kstd::move(other._deleter);
 			return *this;
 		}
 
-		template <typename U, typename E>
-		constexpr unique_ptr &operator=(unique_ptr<U, E> &&other)
-			requires(std::is_convertible_v<U *, T *> && !std::is_array_v<U> &&
-					 std::is_assignable_v<D &, E &&>)
+		template <typename U>
+		constexpr unique_ptr &operator=(unique_ptr<U> &&other)
+			requires(std::is_convertible_v<U *, T *> && !std::is_array_v<U>)
 		{
 			reset(other.release());
-			_deleter = other._deleter;
 			return *this;
 		}
 #pragma endregion
 
 		constexpr ~unique_ptr(void) {
 			if (_ptr != nullptr) {
-				_deleter(_ptr);
+				delete _ptr;
 			}
 		}
 
 		[[nodiscard]] constexpr T *get(void) const {
 			return _ptr;
-		}
-
-		[[nodiscard]] constexpr D &get_deleter(void) {
-			return _deleter;
-		}
-
-		[[nodiscard]] constexpr const D &get_deleter(void) const {
-			return _deleter;
 		}
 
 		[[nodiscard]] constexpr explicit operator bool(void) const {
@@ -224,14 +96,13 @@ namespace kstd {
 
 		constexpr void reset(T *ptr = nullptr) {
 			if (_ptr != nullptr) {
-				_deleter(_ptr);
+				delete _ptr;
 			}
 			_ptr = ptr;
 		}
 
 		constexpr void swap(unique_ptr &other) {
 			kstd::swap(_ptr, other._ptr);
-			kstd::swap(_deleter, other._deleter);
 		}
 	};
 
@@ -260,8 +131,8 @@ namespace kstd {
 	// TODO comparison operators
 	// TODO left shift operator
 
-	template <typename T, typename D>
-	constexpr void swap(unique_ptr<T, D> &lhs, unique_ptr<T, D> &rhs) {
+	template <typename T>
+	constexpr void swap(unique_ptr<T> &lhs, unique_ptr<T> &rhs) {
 		lhs.swap(rhs);
 	}
 
