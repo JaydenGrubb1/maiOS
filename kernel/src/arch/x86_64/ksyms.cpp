6ddfe0cc0dbe3fcb5cca4220681a593f47714b3a
Jayden Grubb <jaydengrubb1@gmail.com>
2024-01-17 03:52:19 +1000

    fix: KSyms use Paging::to_kernel()
    Fixed an issue where KSyms would page fault due to the addresses
    contained in the ELF headers being physical addresses in lower half
    memory.

--- kernel/src/arch/x86_64/ksyms.cpp
+++ kernel/src/arch/x86_64/ksyms.cpp
@@ -18,6 +18,7 @@
 
 #include <arch/x86_64/elf.h>
 #include <arch/x86_64/ksyms.h>
+#include <arch/x86_64/memory/paging.h>
 #include <arch/x86_64/multiboot2.h>
 #include <debug.h>
 
@@ -29,9 +30,9 @@ kstd::pair<const char *, uintptr_t> KSyms::get_symbol(void *addr) {
 		return {nullptr, 0};
 	}
 
+	auto table = reinterpret_cast<ELF::SymbolTableEntry *>(Memory::Paging::to_kernel(symtab->sh_addr));
 	for (size_t i = 0; i < symtab->sh_size / sizeof(ELF::SymbolTableEntry); i++) {
-		auto sym = reinterpret_cast<ELF::SymbolTableEntry *>(symtab->sh_addr)[i];
-
+		auto sym = table[i];
 		if (ELF64_ST_TYPE(sym.st_info) == static_cast<uint8_t>(ELF::SymbolType::STT_FUNC) && sym.st_size != 0) {
 			if (addr >= reinterpret_cast<void *>(sym.st_value) && addr < reinterpret_cast<void *>(sym.st_value + sym.st_size)) {
 				return {&strtab[sym.st_name], sym.st_value};
@@ -76,7 +77,7 @@ void KSyms::init(void) {
 	}
 
 	Debug::log_ok("Found STRTAB section");
-	strtab = reinterpret_cast<char *>(sections[symtab->sh_link].sh_addr);
+	strtab = reinterpret_cast<char *>(Memory::Paging::to_kernel(sections[symtab->sh_link].sh_addr));
 
 	Debug::log_ok("Kernel symbol table initialized");
 }
