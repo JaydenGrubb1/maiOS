601ddccee54f41c047e044b91886b82a77bf00a5
Jayden Grubb <jaydengrubb1@gmail.com>
2024-02-15 17:12:24 +1000

    feat: Add Kernel::panic() function
    Added a Kernel::panic() function. For now, all this does is log a given
    message, print a stack trace and halt the kernel.

---
+++ include/kernel/panic.h
@@ -0,0 +1,24 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2024-02-15
+ * @brief Panic the kernel
+ *
+ * Copyright (c) 2024, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <string_view>
+
+namespace Kernel {
+	/**
+	 * @brief Panic the kernel
+	 *
+	 * @param msg The message to display
+	 */
+	void panic(std::string_view msg);
+}
---
+++ kernel/src/panic.cpp
@@ -0,0 +1,21 @@
+/**
+ * @author Jayden Grubb (contact@jaydengrubb.com)
+ * @date 2024-02-15
+ * @brief Panic the kernel
+ *
+ * Copyright (c) 2024, Jayden Grubb
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <kernel/arch/cpu.h>
+#include <kernel/debug.h>
+#include <kernel/panic.h>
+
+void Kernel::panic(std::string_view msg) {
+	Debug::log_failure("Kernel panic: %s", msg.data());
+	Debug::trace_stack(__builtin_frame_address(0));
+	CPU::halt();
+}
