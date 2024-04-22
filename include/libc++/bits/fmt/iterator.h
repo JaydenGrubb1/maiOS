100b6f47ff9fd94b19ca59fc4f42b674a70a495e
Jayden Grubb <jaydengrubb1@gmail.com>
2024-04-22 12:05:48 +1000

    feat(libc++): Add all remaining format iter sinks

--- include/libc++/bits/fmt/iterator.h
+++ include/libc++/bits/fmt/iterator.h
@@ -1,7 +1,7 @@
 /**
  * @author Jayden Grubb (contact@jaydengrubb.com)
  * @date 2024-04-21
- * @brief Various iterator types for formatting
+ * @brief Various type erased iterators for formatting
  *
  * Copyright (c) 2024, Jayden Grubb
  * All rights reserved.
@@ -12,46 +12,113 @@
 
 #pragma once
 
+#include <bits/algo_basic.h>
+#include <bits/back_insert_iterator.h>
 #include <bits/iterator_traits.h>
 #include <cstdio>
 
 namespace std::__detail {
+	inline constexpr size_t __format_buffer_size = 256;
+
 	template <typename Char>
-	class __format_iter {
-	  public:
-		using value_type = void;
-		using pointer = void;
-		using reference = void;
-		using difference_type = ptrdiff_t;
-		using iterator_category = std::output_iterator_tag;
+	struct __format_buf {
+		using value_type = Char;
 
-		constexpr __format_iter(void) = default;
-		constexpr __format_iter(const __format_iter &) = default;
+		Char *_ptr = nullptr;
+		size_t _size = 0;
+		size_t _capacity = 0;
 
-		constexpr inline __format_iter &operator=(Char c) {
-			std::putchar(c);
-			if (c == '\n') {
-				std::fflush(stdout);
+		explicit constexpr __format_buf(size_t size)
+			: _size(0), _capacity(size) {}
+
+		constexpr __format_buf(Char *ptr, size_t size, size_t capacity)
+			: _ptr(ptr), _size(size), _capacity(capacity) {}
+
+		~__format_buf() = default;
+
+		__format_buf(const __format_buf &) = delete;
+		__format_buf &operator=(const __format_buf &) = delete;
+
+		virtual void grow(size_t new_size) = 0;
+
+		constexpr void push_back(const Char value) {
+			if (_size >= _capacity) {
+				grow(_size + 1);
 			}
-			return *this;
+			_ptr[_size++] = value;
+		}
+	};
+
+	template <typename Iter, typename Char>
+	struct __format_iter_buf : public __format_buf<Char> {
+		using difference_type = iterator_traits<Iter>::difference_type;
+
+		Char _data[__format_buffer_size];
+		Iter _iter;
+		difference_type _max;
+
+		explicit __format_iter_buf(Iter iter, difference_type size = __format_buffer_size)
+			: __format_buf<Char>(_data, 0, __format_buffer_size), _iter(std::move(iter)), _max(size) {}
+
+		~__format_iter_buf() {
+			if (this->_size != 0) {
+				flush();
+			}
 		}
 
-		constexpr inline __format_iter &operator*() {
-			return *this;
+		void flush(void) {
+			auto size = std::min(this->_size, static_cast<size_t>(_max));
+			// _iter = std::copy_n(this->_ptr, size, _iter); // TODO add copy_n
+			for (size_t i = 0; i < size; i++) {
+				*_iter++ = this->_ptr[i];
+			}
+			this->_size = 0;
+			_max -= size;
 		}
 
-		constexpr inline __format_iter &operator++() {
-			return *this;
+		void grow(size_t) override final {
+			if (this->_size != __format_buffer_size) {
+				return;
+			}
+			flush();
 		}
 
-		constexpr inline __format_iter &operator++(int) {
-			return *this;
+		Iter out(void) {
+			flush();
+			return std::move(_iter);
+		}
+
+		difference_type count(void) {
+			return this->_size;
 		}
 	};
 
 	template <typename Char>
-	class __nop_iter {
-	  public:
+	struct __format_count_buf : public __format_buf<Char> {
+		Char _data[__format_buffer_size]; // TODO why does it need any buffer
+		size_t _count = 0;
+
+		__format_count_buf(void)
+			: __format_buf<Char>(_data, 0, __format_buffer_size) {}
+
+		void grow(size_t) override final {
+			if (this->_size != __format_buffer_size) {
+				return;
+			}
+			_count += this->_size;
+			this->_size = 0;
+		}
+
+		size_t count(void) {
+			return _count + this->_size;
+		}
+	};
+
+	template <typename Char>
+	using __format_iter = std::back_insert_iterator<__format_buf<Char>>;
+
+	template <typename Char>
+	struct __nop_iter {
 		using value_type = void;
 		using pointer = void;
 		using reference = void;
